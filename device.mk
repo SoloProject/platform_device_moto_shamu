5cd766d19c5370c0712e565d51fbfc23f73da25c
mike <mikew8978@gmail.com>
2015-10-14 08:59:50 -0400

    Update more props

--- device.mk
+++ device.mk
@@ -170,17 +170,21 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Missing build.props that are present in factory image
 PRODUCT_PROPERTY_OVERRIDES += \
-    fmas.spkr_6ch=35,20,110 \
-    fmas.spkr_2ch=35,25 \
-    fmas.spkr_angles=10 \
-    fmas.spkr_sgain=0 \
-    media.aac_51_output_enabled=true \
     persist.rcs.supported=0 \
     persist.audio.dualmic.config=endfire \
     persist.audio.fluence.voicecall=true \
     persist.audio.fluence.voicerec=false \
     persist.audio.fluence.speaker=false \
     persist.radio.sib16_support=1 \
+    persist.data.qmi.adb_logmask=0 \
+    persist.data.iwlan.enable=true \
+    persist.radio.ignore_ims_wlan=1 \
+    persist.radio.data_con_rprt=1 \
+    fmas.spkr_6ch=35,20,110 \
+    fmas.spkr_2ch=35,25 \
+    fmas.spkr_angles=10 \
+    fmas.spkr_sgain=0 \
+    media.aac_51_output_enabled=true \
     ro.facelock.black_timeout=400 \
     ro.facelock.det_timeout=1500 \
     ro.facelock.rec_timeout=250 \
